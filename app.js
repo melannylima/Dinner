c81547113433f6b323618d0094687f5d31606a1b
Melanny <melanny.lima@outlook.com>
2022-08-23 21:36:25 -0400

    mongo connected

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+// MONGOOSE
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/dinnerapp')
+mongoose.connection.once('open', () => {
+  console.log('connected to mongo');
+})
+
 // MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -9,6 +17,12 @@ app.use(express.urlencoded({extended: true}))
 app.get('/wfd', (req, res) => {
   console.log('index');
   res.render('index.ejs')
+})
+
+// NEW
+app.get('/wfd/newp', (req, res) => {
+  console.log('new pantry item');
+  res.send('new pantry item')
 })
 
 app.listen(3000, () => {
