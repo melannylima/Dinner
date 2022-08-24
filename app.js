e1822c59a23be4801f4f70ceaf98529b526f6f36
Melanny <melanny.lima@outlook.com>
2022-08-23 21:03:20 -0400

    nodemon works

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+})
