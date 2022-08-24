cb2cdda51bc88a32a0a4dd1a7f71efb62666a957
Melanny <melanny.lima@outlook.com>
2022-08-23 21:07:41 -0400

    index route works

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// INDEX
+app.get('/wfd', (req, res) => {
+  console.log('index');
+  res.render('index.ejs')
+})
+
 app.listen(3000, () => {
   console.log(`listening on port 3000`);
 })
